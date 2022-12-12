48c30500de35a8016546e7220a089a47abe4cc71
Vellyus <noisyboy026@gmail.com>
2022-12-12 01:32:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Button from "@mui/material/Button"
+import SaveIcon from "@mui/icons-material/Save"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={ logo } className="App-logo" alt="logo" />
+
+        <Button
+          startIcon={ <SaveIcon /> }
+          size="large"
+          variant="contained"
+          color="secondary">
+          Hello World
+        </Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
